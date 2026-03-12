de497c6ef01bd188ead7d8fae4ae4897024ce940
hasan <hasan.hatipoglu.a@gmail.com>
2026-03-12 03:59:41 +0100

    use 48000 sample rate

--- convert_mp3_to_raw.sh
+++ convert_mp3_to_raw.sh
@@ -7,6 +7,9 @@ for file in records/*; do
     if [ -f "$file" ]; then
         echo "Processing $file"
         filename=$(basename "$file" .mp3)
-        ffmpeg -i "$file" -ar 16000 -f s16le "$RAW_DIR/$filename.raw"
+        # sample rate is 48000
+        # -ac (2 channel/stereo)
+        # output format -f s16le (raw signed 16-bit little endian)
+        ffmpeg -i "$file" -ar 48000 -ac 2 -f s16le "$RAW_DIR/$filename.raw"
     fi
 done
