d08935dd679c84e9f1df1cef5f26a33f3b4d8e85
hasan <hasan.hatipoglu.a@gmail.com>
2026-03-13 00:22:23 +0100

    add all sounds with build.rs

--- kirov-reporting/build.rs
+++ kirov-reporting/build.rs
@@ -1,3 +1,16 @@
+
 fn main() {
     embuild::espidf::sysenv::output();
+    let dir = std::fs::read_dir("../raw_records").unwrap();
+    let mut paths: Vec<_> = dir
+        .filter_map(|e| e.ok())
+        .map(|e| e.path().canonicalize().unwrap())
+        .filter(|p| p.extension().map(|e| e == "raw").unwrap_or(false)) 
+        .collect();
+    paths.sort();
+
+    for (i, path) in paths.iter().enumerate() {
+        println!("cargo:rustc-env=SOUND_{}={}", i, path.display());
+    }
+    println!("cargo:rustc-env=SOUND_COUNT={}", paths.len());
 }
--- kirov-reporting/src/main.rs
+++ kirov-reporting/src/main.rs
@@ -8,12 +8,12 @@ use esp_idf_svc::hal::prelude::*;
 use std::time::Duration;
 
 static BUTTON_PRESSED: AtomicBool = AtomicBool::new(false);
-const MAX_COUNT: u32 = 11;
-const ES8311_ADDR: u8 = 0x18;
-
 
 // https://files.waveshare.com/wiki/common/ES8311.user.Guide.pdf
 fn es8311_init(i2c: &mut I2cDriver) -> Result<(), Box<dyn std::error::Error>> {
+
+    const ES8311_ADDR: u8 = 0x18;
+
     i2c.write(ES8311_ADDR,&[0x00, 0x80],1000)?; // reset reg, csm power om
     i2c.write(ES8311_ADDR,&[0x01, 0x95],1000)?; // clk maanager reg, from bclk, mlk offm bclk on, reset master, reset dac
     i2c.write(ES8311_ADDR,&[0x02, 0x18],1000)?; // clk maanager reg, mclk_prediv = 3
@@ -31,11 +31,45 @@ fn es8311_init(i2c: &mut I2cDriver) -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+fn play_record(sound : &[u8], i2s :&mut I2sDriver<I2sTx>, amp_enable :&mut PinDriver<Gpio18,Output>) -> Result<(), Box<dyn std::error::Error>>
+{
+    const CHUNK_SIZE : usize = 4096;
+    let mut offset: usize = 0;
+
+    amp_enable.set_high()?;
+    std::thread::sleep(Duration::from_millis(200));
+    
+    while offset < sound.len() {
+        let end = (offset + CHUNK_SIZE).min(sound.len());
+        i2s.write(&sound[offset..end], 1000)?;
+        offset = end;
+    }
+
+    amp_enable.set_low()?;
+
+    Ok(())
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    const SOUND: &[u8] = include_bytes!(env!("SOUND_FILE"));
+
+    const SOUNDS: &[&[u8]] = &[
+        include_bytes!(env!("SOUND_0")),
+        include_bytes!(env!("SOUND_1")),
+        include_bytes!(env!("SOUND_3")),
+        include_bytes!(env!("SOUND_4")),
+        include_bytes!(env!("SOUND_5")),
+        include_bytes!(env!("SOUND_6")),
+        include_bytes!(env!("SOUND_7")),
+        include_bytes!(env!("SOUND_8")),
+        include_bytes!(env!("SOUND_9")),
+        include_bytes!(env!("SOUND_10")),
+    ];
+
+    const MAX_COUNT: usize = SOUNDS.len();
+
     let peripherals = Peripherals::take().unwrap();
 
     let mut amp_enable = PinDriver::output(peripherals.pins.gpio18)?;
@@ -66,34 +100,28 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     button.set_pull(Pull::Up)?;
     button.set_interrupt_type(InterruptType::NegEdge)?;
 
-    let mut count: u32 = 0;
+    let mut count: usize = 0;
     unsafe {
         button.subscribe(|| {
             BUTTON_PRESSED.store(true, Ordering::Relaxed);
         })?;
     }
+
     button.enable_interrupt()?;
+
+    play_record(SOUNDS[0], &mut i2s, &mut amp_enable)?;
 
     loop {
         if BUTTON_PRESSED.load(Ordering::Relaxed) {
             BUTTON_PRESSED.store(false, Ordering::Relaxed);
             println!("Kirov Reporting {}", count);
+
+            play_record(SOUNDS[count], &mut i2s, &mut amp_enable)?;
+
             count += 1;
             if count >= MAX_COUNT { 
-                count = 0; 
+                count=0;
             }
-
-            amp_enable.set_high()?;
-            std::thread::sleep(Duration::from_millis(200));
-
-            let chunk_size = 4096;
-            let mut offset = 0;
-            while offset < SOUND.len() {
-                let end = (offset + chunk_size).min(SOUND.len());
-                i2s.write(&SOUND[offset..end], 1000)?;
-                offset = end;
-            }
-            amp_enable.set_low()?;
             button.enable_interrupt()?;
         }
         std::thread::sleep(Duration::from_millis(10));
